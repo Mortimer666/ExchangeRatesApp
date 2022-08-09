00d4c25f42779e37016efd5f97f4fccd3de7be14
Artur Litviniuk <Arturio1366@inbox.ru>
2022-08-09 18:38:49 +0300

    Stub in tests

--- src/test/java/com/litviniuk/exchangerates/ExchangeRatesAppApplicationTests.java
+++ src/test/java/com/litviniuk/exchangerates/ExchangeRatesAppApplicationTests.java
@@ -1,22 +1,12 @@
 package com.litviniuk.exchangerates;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExchangeRatesAppApplicationTests {
 
-    @Autowired
-    private ObjectMapper objectMapper = new ObjectMapper();
-
     @Test
-    void shouldThrow() {
-//        String test = "{\"name\":\"value\"}";
-//        RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
-//        Mockito.when(restTemplate.getForObject("1", String.class)).thenReturn(("f"));
-//        Assertions.assertThrows(TroublesWithJsonException.class, () -> objectMapper.readTree(test));
+    void stub() {
     }
-
 }
